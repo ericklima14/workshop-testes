3b241d3c24d186801304edea9e2ab16030a86975
andreza marianno <mariannoandreza@gmail.com>
2025-05-12 21:05:08 -0300

    Initial Commit

---
+++ Testes Unitarios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testes Unitarios
+//
+//  Created by AppleAcademy1 on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testes Unitarios/Testes_UnitariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testes_UnitariosApp.swift
+//  Testes Unitarios
+//
+//  Created by AppleAcademy1 on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Testes_UnitariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
